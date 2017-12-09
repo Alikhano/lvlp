f320f127563be4ab6e3e1640ec08e25684ccfbfa
alikhano <nannette121@gmail.com>
2017-12-10 00:52:26 +0300

    test corrections commit

--- src/test/java/task3/LoginProcessorTest.java
+++ src/test/java/task3/LoginProcessorTest.java
@@ -1,8 +1,11 @@
 package task3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class LoginProcessorTest {
 
